b33e775dbe2dda5fb5a692befa1b334259184857
BlankWareDev <blankwaredev@protonmail.com>
2019-10-23 03:52:34 -0400

    Add project files.

---
+++ WpfBug/Employee.cs
@@ -0,0 +1,96 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfBug
+{
+    public class Employee : ObservableObject
+    {
+        private int id;
+        private string name;
+        private int age;
+        private decimal salary;
+        private bool checkMe;
+
+        public bool CheckMe
+        {
+            get
+            {
+                return checkMe;
+            }
+            set
+            {
+                Set<bool>(() => this.CheckMe, ref checkMe, value);
+            }
+        }
+
+        public int ID
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                Set<int>(() => this.ID, ref id, value);
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                Set<string>(() => this.Name, ref name, value);
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                Set<int>(() => this.Age, ref age, value);
+            }
+        }
+
+        public decimal Salary
+        {
+            get
+            {
+                return salary;
+            }
+            set
+            {
+                Set<decimal>(() => this.Salary, ref salary, value);
+            }
+        }
+
+        public static ObservableCollection<Employee> GetSampleEmployees()
+        {
+            ObservableCollection<Employee> employees = new ObservableCollection<Employee>();
+            for (int i = 0; i < 30; ++i)
+            {
+                employees.Add(new Employee
+                {
+                    checkMe = false,
+                    ID = i + 1,
+                    Name = "Name " + (i + 1).ToString(),
+                    Age = 20 + i,
+                    Salary = 20000 + (i * 10)
+                });
+            }
+            return employees;
+        }
+    }
+}
